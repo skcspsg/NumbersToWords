d7c0983c6efba19f12d4397c98579f868f6cb3b7
skcspsg <60703204+skcspsg@users.noreply.github.com>
2020-02-06 21:44:57 +0530

    convertsSingleDigits test case update

--- src/test/java/com/belgium/numtoword/business/strategy/NumbersToWordsTest.java
+++ src/test/java/com/belgium/numtoword/business/strategy/NumbersToWordsTest.java
@@ -21,5 +21,17 @@ public class NumbersToWordsTest {
 	  public void convertsZero() {
 	    expectConversion(0, "zero");
 	  }
+	@Test
+	  public void convertsSingleDigits() {
+	    expectConversion(1, "one");
+	    expectConversion(2, "two");
+	    expectConversion(3, "three");
+	    expectConversion(4, "four");
+	    expectConversion(5, "five");
+	    expectConversion(6, "six");
+	    expectConversion(7, "seven");
+	    expectConversion(8, "eight");
+	    expectConversion(9, "nine");
+	  }
 
 }
