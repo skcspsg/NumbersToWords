888c471a7c018d5907d7184c8d4e2dc3a91c127b
skcspsg <60703204+skcspsg@users.noreply.github.com>
2020-02-06 21:46:29 +0530

    NumberTowords Initialization

--- src/main/java/com/belgium/numtoword/business/strategy/NumbersToWords.java
+++ src/main/java/com/belgium/numtoword/business/strategy/NumbersToWords.java
@@ -11,6 +11,23 @@ import com.belgium.numtoword.business.exception.BusinessException;
  */
 
 public class NumbersToWords {
-	
+	private static final String[] NUMBERS_UP_TO_19 = { "zero", "one", "two",
+	      "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+	      "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+	      "seventeen", "eighteen", "nineteen" };
+	  private static final String[] MULTIPLES_OF_10 = { "", "", "twenty", "thirty",
+	      "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+	  
+	  private final Integer number;
+	  private StringBuffer result = new StringBuffer();
+
+	  public static String convert(final Integer number) throws BusinessException {
+	    return new NumbersToWords(number).convert();
+	  }
+
+	  public NumbersToWords(final Integer number) {
+	    this.number = number;
+	  }
+
 }
 
